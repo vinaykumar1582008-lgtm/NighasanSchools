6492fa06150717edb6b1a91839df3ff075a9f826
vinay kumar <vinaykumar1582008@gmail.com>
2026-07-27 04:14:07 +0530

    Use Render Secret File

--- firebase_config.py
+++ firebase_config.py
@@ -1,17 +1,8 @@
 import os
-import json
 import firebase_admin
 from firebase_admin import credentials, auth
 
 if not firebase_admin._apps:
-    if "FIREBASE_KEY" in os.environ:
-        cred_dict = json.loads(os.environ["FIREBASE_KEY"])
-        cred = credentials.Certificate(cred_dict)
-        firebase_admin.initialize_app(cred)
-
-    elif os.path.exists("firebase_key.json"):
-        cred = credentials.Certificate("firebase_key.json")
-        firebase_admin.initialize_app(cred)
-
-    else:
-        print("Firebase key not found. Firebase OTP disabled.")
+    path = os.getenv("FIREBASE_KEY_PATH", "firebase_key.json")
+    cred = credentials.Certificate(path)
+    firebase_admin.initialize_app(cred)
