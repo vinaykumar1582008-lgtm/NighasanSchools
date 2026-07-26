91e1a8b347c67cbc7ebce00dd162e3c2dc6397bd
vinay kumar <vinaykumar1582008@gmail.com>
2026-07-27 03:49:20 +0530

    Support Render Firebase environment key

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,17 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, auth
-import os
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
-    firebase_admin.initialize_app(cred)
+    if "FIREBASE_KEY" in os.environ:
+        cred_dict = json.loads(os.environ["FIREBASE_KEY"])
+        cred = credentials.Certificate(cred_dict)
+        firebase_admin.initialize_app(cred)
+
+    elif os.path.exists("firebase_key.json"):
+        cred = credentials.Certificate("firebase_key.json")
+        firebase_admin.initialize_app(cred)
+
+    else:
+        print("Firebase key not found. Firebase OTP disabled.")
