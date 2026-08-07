8b8a8db33b46de90ee2d109a2f3e9552be8cfba4
vinay kumar <vinaykumar1582008@gmail.com>
2026-08-07 18:05:09 +0530

    Fix admin login

--- frontend/admin/login.js
+++ frontend/admin/login.js
@@ -1,7 +1,9 @@
 const API = "https://nighasanschools.onrender.com";
 
 async function login() {
+
     try {
+
         const username = document.getElementById("username").value.trim();
         const password = document.getElementById("password").value;
 
@@ -16,23 +18,29 @@ async function login() {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                username,
-                password
+                username: username,
+                password: password
             })
         });
 
         const data = await response.json();
 
         if (response.ok) {
+
             localStorage.setItem("admin_token", data.access_token);
-            alert("Login Successful");
+
             window.location.href = "dashboard.html";
+
         } else {
-            alert(data.detail || "Login Failed");
+
+            alert(data.detail);
+
         }
 
-    } catch (err) {
-        console.log(err);
-        alert("Error: " + err.message);
+    } catch (e) {
+
+        alert(e);
+
     }
+
 }
